fc80771bdcebecab7b25e96145f1c6d12e046de2
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-08-14 18:42:45 -0300

    Adding one comment for problem 41

--- src/PartFour.hs
+++ src/PartFour.hs
@@ -83,5 +83,6 @@ problem40 n = head [(a, b) | a <- primes, b <- primes, a + b == n]
 problem41 :: Integral a => a -> a -> [(a, a)]
 problem41 x y = map problem40 $ filter even [x .. y]
 
+-- Very rarely, the primes are both bigger than say 50. Try to find out how many such cases there are in the range 2..3000. 
 problem41' :: Integral a => a -> a -> a -> [(a, a)]
 problem41' x y m = filter (\(a, b) -> a > m && b > m) $ problem41 x y
